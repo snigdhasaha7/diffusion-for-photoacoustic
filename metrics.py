a4065c0334f4eddf1825fac5927f10af99ec100b
Snigdha Saha <snigdhas2572@gmail.com>
2022-11-08 16:37:41 -0800

    update to use skimage for psnr

--- metrics.py
+++ metrics.py
@@ -1,17 +1,15 @@
 import numpy as np
 from math import log10, sqrt
 from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import peak_signal_noise_ratio as psnr
+
 # lower --> more similar images
 def pixel_to_pixel_MSE(original, noisy): 
     return np.mean((original - noisy)**2)
 
 # higher --> more similar images
 def PSNR(original, noisy):
-    mse = pixel_to_pixel_MSE(original, noisy)
-    if mse == 0:
-        return 100 
-    max_pixel = 255.0
-    return 20 * log10(max_pixel / sqrt(mse))
+    return psnr(original, noisy)
 
 # higher --> more similar images
 def SSIM(original, noisy):
