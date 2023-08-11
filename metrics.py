f6080be86f774633a63c2363b312917cfea7a3f5
sreemanti-dey <56366201+sreemanti-dey@users.noreply.github.com>
2023-08-11 15:32:08 -0700

    added avg psnr function and updated psnr to take torch tensors

--- metrics.py
+++ metrics.py
@@ -9,7 +9,18 @@ def pixel_to_pixel_MSE(original, noisy):
 
 # higher --> more similar images
 def PSNR(original, noisy):
+    if not isinstance(original, np.ndarray):
+      original = original.cpu().numpy()
+    if not isinstance(noisy, np.ndarray):
+      noisy = noisy.cpu().numpy()
     return psnr(original, noisy)
+
+def avg_PSNR(GT, preds):
+  # GT and preds must have the same shape
+  psnrs = []
+  for i in range(len(GT)):
+    psnrs.append(PSNR(GT[i], preds[i]))
+  return sum(psnrs) / len(psnrs)
 
 # higher --> more similar images
 def SSIM(original, noisy):
