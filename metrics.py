c5fda8e3e1dbf2a27485fc371138df25502566a0
sreemanti-dey <56366201+sreemanti-dey@users.noreply.github.com>
2023-08-19 23:33:56 -0700

    added a PSNR comparison method

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import log10, sqrt
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
+from statistics import mean, stdev
 
 # lower --> more similar images
 def pixel_to_pixel_MSE(original, noisy): 
@@ -20,7 +21,20 @@ def avg_PSNR(GT, preds):
   psnrs = []
   for i in range(len(GT)):
     psnrs.append(PSNR(GT[i], preds[i]))
-  return sum(psnrs) / len(psnrs)
+  return mean(psnrs), stdev(psnrs)
+
+def compare_PSNR(GT_all, fista_all, sau_all, diff_all):
+  counter = 0
+  for i in range(GT_all.shape[0]):
+    GT = GT_all[i]
+    fista = PSNR(GT, fista_all[i])
+    sau = PSNR(GT, sau_all[i])
+    diff = PSNR(GT, diff_all[i])
+
+    if diff > fista and diff > sau:
+      counter += 1
+  
+  return (counter / GT_all.shape[0]) * 100
 
 # higher --> more similar images
 def SSIM(original, noisy):
@@ -35,4 +49,4 @@ def avg_SSIM(GT, preds):
   ssims = []
   for i in range(len(GT)):
     ssims.append(SSIM(GT[i], preds[i]))
-  return sum(ssims) / len(ssims)
+  return mean(ssims), stdev(ssims)
