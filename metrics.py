8b23b4b351c78aeb04139d4941424cc76adc3420
sreemanti-dey <56366201+sreemanti-dey@users.noreply.github.com>
2023-08-30 00:02:47 -0700

    added lpips metric

--- metrics.py
+++ metrics.py
@@ -1,8 +1,16 @@
 import numpy as np
+import torch
+from torch.serialization import normalize_storage_type 
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
 from math import log10, sqrt
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from statistics import mean, stdev
+
+
+# normalize to [0,1]
+def scale(x):
+  return (x-np.min(x))/(np.max(x)-np.min(x))
 
 # lower --> more similar images
 def pixel_to_pixel_MSE(original, noisy): 
@@ -50,3 +58,17 @@ def avg_SSIM(GT, preds):
   for i in range(len(GT)):
     ssims.append(SSIM(GT[i], preds[i]))
   return mean(ssims), stdev(ssims)
+
+
+def LPIPS(original, noisy, net='alex'):
+    # expecting original and noisy to be NxHxW
+    if not torch.is_tensor(original):
+      original = torch.tensor(scale(original))
+    if not torch.is_tensor(noisy):
+      noisy = torch.tensor(scale(noisy))
+
+    original = torch.tile(torch.unsqueeze(original, 1), (1,3,1,1))
+    noisy = torch.tile(torch.unsqueeze(noisy, 1), (1,3,1,1))
+    lpips = LearnedPerceptualImagePatchSimilarity(net_type=net, normalize=True)
+    return lpips(original, noisy).item()
+    
